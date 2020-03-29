7e0da2968668a97f24777488a80b08e00cf19260
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-03-29 23:51:29 +0530

    Added difficulty modes.

--- app/src/main/java/com/amanaggarwal1/dothemath/DifficultySelectionActivity.java
+++ app/src/main/java/com/amanaggarwal1/dothemath/DifficultySelectionActivity.java
@@ -3,10 +3,19 @@ package com.amanaggarwal1.dothemath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SearchRecentSuggestionsProvider;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DifficultySelectionActivity extends AppCompatActivity {
+
+    ListView difficultyLV;
 
     public void startGameButtonClicked(View view){
         Intent intent = new Intent(this, GameActivity.class);
@@ -14,10 +23,33 @@ public class DifficultySelectionActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void setDifficultyModes(){
+
+
+        ArrayList<String> difficultyModes = new ArrayList<>();
+        difficultyModes.add("Beginner");
+        difficultyModes.add("Easy");
+        difficultyModes.add("Medium");
+        difficultyModes.add("Hard");
+        difficultyModes.add("Very Hard");
+        difficultyModes.add("Pro Grade");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, difficultyModes);
+        difficultyLV.setAdapter(arrayAdapter);
+        difficultyLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,long arg3) {
+                view.setSelected(true);
+            }
+        });
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty_selection);
 
+        difficultyLV = findViewById(R.id.difficultyLV);
     }
 }
